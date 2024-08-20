54fa473cacbd60a774c8910e1a5c342f8343f69c
Samir Jabre <73381924>
2024-08-20 18:12:27 +0300

    Finished the history migration

--- database/migrations/2024_08_20_150650_history.php
+++ database/migrations/2024_08_20_150650_history.php
@@ -11,7 +11,20 @@ return new class extends Migration
      */
     public function up(): void
     {
-        //
+        Schema::create('history', function (Blueprint $table) {
+            $table->id()->primaryKey();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
+            $table->unsignedBigInteger('driver_id');
+            $table->foreign('driver_id')->references('id')->on('drivers')->onDelete('cascade')->onUpdate('cascade');
+            $table->unsignedBigInteger('bus_id');
+            $table->foreign('bus_id')->references('id')->on('buses')->onDelete('cascade')->onUpdate('cascade');
+            $table->json('routes_taken')->nullable();
+            $table->string('from')->nullable();
+            $table->string('to')->nullable();
+            $table->rememberToken();
+            $table->timestamps();
+        });
     }
 
     /**
