7f998fec243c07e56892cfb87b0c50fbcc54dfea
Samir Jabre <73381924>
2024-08-20 17:10:46 +0300

    Created User Migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,8 +15,11 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('profile_picture')->nullable();
+            $table->json('reviews')->nullable();
+            $table->json('trips_history')->nullable();
+            $table->json('preferred_routes')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
