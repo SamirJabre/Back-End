1eea73aea495f811443215868eb9724c782b546e
Samir Jabre <73381924>
2024-08-26 17:50:30 +0300

    Added the otp in the account creation function

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -53,6 +53,7 @@ class AuthController extends Controller
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
+            'otp' => $otp,
         ]);
 
         $token = Auth::login($user);
