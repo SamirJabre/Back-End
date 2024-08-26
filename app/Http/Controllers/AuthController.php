0aa1ca42ce0606a26e613ed18f43ce7913fb7f4d
Samir Jabre <73381924>
2024-08-26 17:49:53 +0300

    Added the method to generate the 4 digits otp

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -48,7 +48,7 @@ class AuthController extends Controller
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:6',
         ]);
-
+        $otp = random_int(1000, 9999);
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
