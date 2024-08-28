0d865b4f09b62e3fccfec85e4886894d9e35e693
Samir Jabre <73381924>
2024-08-28 20:19:37 +0300

    Finished the DeactivatingAccounts Controller

--- app/Http/Controllers/DeactivatingAccounts.php
+++ app/Http/Controllers/DeactivatingAccounts.php
@@ -6,5 +6,16 @@ use Illuminate\Http\Request;
 
 class DeactivatingAccounts extends Controller
 {
-    //
+    public function deleteUnverifiedUsers()
+    {
+        // Fetch unverified users
+        $unverifiedUsers = User::where('is_verified', false)->get();
+
+        // Delete unverified users
+        foreach ($unverifiedUsers as $user) {
+            $user->delete();
+        }
+
+        return response()->json(['message' => 'Unverified users deleted successfully.']);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DeactivatingAccounts;
 use App\Http\Controllers\OtpController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
