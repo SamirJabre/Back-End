f40b1850a5eaccbd89d344151c361303697b0d07
Samir Jabre <73381924>
2024-08-28 20:20:29 +0300

    Adding the route

--- routes/api.php
+++ routes/api.php
@@ -27,3 +27,4 @@ Route::controller(AuthController::class)->group(function () {
 });
 
 Route::post('/validate-otp', [OtpController::class, 'validateOtp']);
+Route::get('/delete-unverified', [DeactivatingAccounts::class, 'deleteUnverifiedUsers']);
