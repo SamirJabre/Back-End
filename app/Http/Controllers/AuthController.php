f96e2b17fdc59cbcf1128f1cc05843fd30c32c9a
Samir Jabre <73381924>
2024-08-28 20:07:53 +0300

    Fixing Bugs

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -106,37 +106,7 @@ class AuthController extends Controller
     }
 
 
-    public function validateOtp(Request $request){
-        $request->validate([
-            'email' => 'required|string|email|max:255',
-            'otp' => 'required|integer',
-        ]);
     
-        $user = User::where('email', $request->email)->first();
-    
-        if (!$user) {
-            return response()->json([
-                'status' => 'error',
-                'message' => 'User not found',
-            ], 404);
-        }
-    
-        if ($user->otp == $request->otp) {
-            $user->is_verified = true;
-            $user->email_verified_at = now();
-            $user->save();
-    
-            return response()->json([
-                'status' => 'success',
-                'message' => 'OTP verified successfully. User is now verified.',
-            ]);
-        } else {
-            return response()->json([
-                'status' => 'error',
-                'message' => 'Invalid OTP',
-            ], 400);
-        }
-    }
 
 
 
